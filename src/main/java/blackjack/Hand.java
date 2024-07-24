c553269a819980e66463272de3c4166b4122addf
Masayuki-Kanzaki <mk199967@gmail.com>
2024-07-24 23:42:35 +0900

    Refactoring Hand class sumPoints method

--- src/main/java/blackjack/Hand.java
+++ src/main/java/blackjack/Hand.java
@@ -15,12 +15,6 @@ public class Hand {
         this.cards = new ArrayList<>();
     }
 /* 
-    public Hand (List<Card> card) {
-        this.cards = card;
-        this.points = 2;
-    }
-*/
-/* 
     public Hand add(Card card) {
         List<Card> result = new ArrayList<>(cards);
         result.add(card);
@@ -29,7 +23,6 @@ public class Hand {
 */
     public void addCard(Card card) {
         cards.add(card);
-//        points += card.getCardTypePoint();
     }
 /* 
     public void showCards() {
@@ -42,33 +35,50 @@ public class Hand {
         return sumPoints();
     }
 
-    public int sumPoints() {
-        int points = 0;
+    public int countAce() {
         int countAce = 0;
-        int countTenCard = 0;
         for (int i = 0; i < cards.size(); i++) {
             Card card = cards.get(i);
-            points += card.getCardTypePoint();
             if (card.isAce()) {
                 countAce += 1;
             }
+        }
+        return countAce;
+    }
+
+    public int countTenCard() {
+        int countTenCard = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
             if (card.isTenCard()) {
                 countTenCard += 1;
             }
         }
+        return countTenCard;
+    }
 
-        for (int i = 0; i < countAce; i++) {
-            if (points >= MAX) {
+    public int sumPointsWithoutAce() {
+        int points = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
+            points += card.getCardTypePoint();
+        }
+        return points;
+    }
+/* 
+    public boolean isAboveTwentyOne() {
+        return bust;
+    }
+*/
+    public int sumPoints() {
+        int points = sumPointsWithoutAce();
+        for (int i = 0; i < countAce(); i++) {
+            if ((points % 21 <= 20) && (points % 21 >= 11)) {
                 points += 1;
-                bust = true;
-                continue;
-            } else if ((points % 21 <= 20) && (points % 21 >= 11)) {
-                points += 1;
-            } else if ((points % 21 == 10) && (countTenCard == 1)){
+            } else if ((points % 21 == 10) && (countTenCard() == 1)){
                 points += 11;
                 blackjack = true;
             } else {
-                // もう少しケース分岐の検討要
                 points += 11;
             }
         }
@@ -84,4 +94,7 @@ public class Hand {
         return bust;
     }
 
+    public boolean isBlackjack() {
+        return blackjack;
+    }
 }
--- src/test/java/blackjack/HandTest.java
+++ src/test/java/blackjack/HandTest.java
@@ -14,7 +14,57 @@ public class HandTest {
         Assert.assertEquals(4, hand2.showPoints());
     }
 */
-@Test
+    @Test
+    public void count_Ace() {
+        Hand hand = new Hand();
+        hand.addCard(CardFactory.createCard(CardType.ace, CardMark.diamond));
+        Assert.assertEquals(1, hand.countAce());
+        hand.addCard(CardFactory.createCard(CardType.two, CardMark.diamond));
+        Assert.assertEquals(1, hand.countAce());
+        hand.addCard(CardFactory.createCard(CardType.ace, CardMark.heart));
+        Assert.assertEquals(2, hand.countAce());
+
+        Hand hand2 = new Hand();
+        hand2.addCard(CardFactory.createCard(CardType.two, CardMark.diamond));
+        Assert.assertEquals(0, hand2.countAce());
+    }
+
+    @Test
+    public void count_TenCard() {
+        Hand hand = new Hand();
+        hand.addCard(CardFactory.createCard(CardType.ten, CardMark.diamond));
+        Assert.assertEquals(1, hand.countTenCard());
+        hand.addCard(CardFactory.createCard(CardType.nine, CardMark.diamond));
+        Assert.assertEquals(1, hand.countTenCard());
+        hand.addCard(CardFactory.createCard(CardType.jack, CardMark.heart));
+        Assert.assertEquals(2, hand.countTenCard());
+
+        Hand hand2 = new Hand();
+        hand2.addCard(CardFactory.createCard(CardType.ace, CardMark.diamond));
+        Assert.assertEquals(0, hand2.countTenCard());
+    }
+
+    @Test
+    public void sum_Points_WithoutAce() {
+        Hand hand = new Hand();
+        hand.addCard(CardFactory.createCard(CardType.ten, CardMark.diamond));
+        hand.addCard(CardFactory.createCard(CardType.eight, CardMark.diamond));
+        Assert.assertEquals(18, hand.sumPointsWithoutAce());
+        hand.addCard(CardFactory.createCard(CardType.two, CardMark.diamond));
+        Assert.assertEquals(20, hand.sumPointsWithoutAce());
+
+        Hand hand2 = new Hand();
+        hand2.addCard(CardFactory.createCard(CardType.ace, CardMark.diamond));
+        Assert.assertEquals(0, hand2.sumPointsWithoutAce());
+        hand2.addCard(CardFactory.createCard(CardType.jack, CardMark.diamond));
+        Assert.assertEquals(10, hand2.sumPointsWithoutAce());
+        hand2.addCard(CardFactory.createCard(CardType.queen, CardMark.diamond));
+        Assert.assertEquals(20, hand2.sumPointsWithoutAce());
+        hand2.addCard(CardFactory.createCard(CardType.ace, CardMark.diamond));
+        Assert.assertEquals(20, hand2.sumPointsWithoutAce());
+    }
+
+    @Test
     public void sum_Points() {
         Hand hand3 = new Hand();
         hand3.addCard(CardFactory.createCard(CardType.ace, CardMark.diamond));
@@ -47,5 +97,23 @@ public class HandTest {
         hand3.addCard(CardFactory.createCard(CardType.two, CardMark.heart));
         hand3.addCard(CardFactory.createCard(CardType.eight, CardMark.heart));
         Assert.assertEquals(21, hand3.sumPoints());
+        Assert.assertFalse(hand3.isBlackjack());
+    }
+    @Test
+    public void sum_Points5_for_blackjack() {
+        Hand hand5 = new Hand();
+        hand5.addCard(CardFactory.createCard(CardType.ace, CardMark.diamond));
+        hand5.addCard(CardFactory.createCard(CardType.jack, CardMark.diamond));
+        Assert.assertEquals(21, hand5.sumPoints());
+        Assert.assertTrue(hand5.isBlackjack());
+    }
+    @Test
+    public void sum_Points6_for_bust() {
+        Hand hand6 = new Hand();
+        hand6.addCard(CardFactory.createCard(CardType.ten, CardMark.diamond));
+        hand6.addCard(CardFactory.createCard(CardType.jack, CardMark.diamond));
+        hand6.addCard(CardFactory.createCard(CardType.queen, CardMark.diamond));
+        Assert.assertEquals(30, hand6.sumPoints());
+        Assert.assertTrue(hand6.isBust());
     }
 }
